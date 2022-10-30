74ccb99412b42491a018a686fc991cf73a5b47e7
clanlop <clanlop@hotmail.com>
2022-10-29 22:05:09 -0300

    agregue mock de tareas y el interface

---
+++ src/app/components/mock-tasks.ts
@@ -0,0 +1,35 @@
+import{Task}from"./Task"
+
+export const TASKS: Task[]=[
+    {
+      id:1,
+      text:"Terminar primer modulo de Angular",
+      day:"Octubre 25 a las 12:00hs",
+      reminder:true
+
+    },
+    {
+        id:2,
+        text:"Hacer las compras para la cena",
+        day:"Octubre 25 a las 15:00hs",
+        reminder:true
+  
+    },
+
+    {
+      id:3,
+      text:"Investigar sobre Boostrap",
+      day:"Octubre 25 a las 17:00hs",
+      reminder:false
+
+    },
+
+    {
+        id:4,
+        text:"Leer mi libro",
+        day:"Octubre 25 a las 20:00hs",
+        reminder:false
+  
+    }
+
+]
